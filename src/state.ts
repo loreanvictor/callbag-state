3ead3f2498012bc331d21706fbe4925cb82148d5
Eugene Ghanizadeh <ghanizadeh.eugene@gmail.com>
2020-12-06 13:25:35 +0100

    Update state.ts

--- src/state.ts
+++ src/state.ts
@@ -9,6 +9,13 @@ import { broadcast } from './util/broadcast';
 import { postTrace } from './trace';
 
 
+/**
+ *
+ * sends an END signal to all sinks, with given error as reason.
+ * if a talkback is provided, will also send an END signal upwards to the
+ * talkback.
+ *
+ */
 function terminate<T>(sinks: Sink<Change<T>>[], talkback: any, err?: any) {
   broadcast(_End, err, sinks);
   sinks.length = 0;
@@ -17,11 +24,53 @@ function terminate<T>(sinks: Sink<Change<T>>[], talkback: any, err?: any) {
   }
 }
 
+/**
+ *
+ * returns the latest value of the state.
+ *
+ */
 function get<T>(this: State<T>) { return this(_Latest as any) as any as T; }
+
+/**
+ *
+ * updates state's value.
+ *
+ */
 function set<T>(this: State<T>, t: T) { this(_Data, t); }
+
+/**
+ *
+ * clears the state (sending an end signal to all sinks, clearing memory).
+ *
+ */
 function clear<T>(this: State<T>) { this(_End); }
+
+/**
+ *
+ * returns the downstream of the state. the downstream is a read stream of changes
+ * (NOT values) that happen to the state.
+ *
+ */
 function getDownstream<T>(this: State<T>) { return this(_Downstream as any) as any as Downstream<T>; }
+
+/**
+ *
+ * returns the upstream of the state. the upstream is a write stream through
+ * which the state can request changes (NOT values) to be applied to it.
+ *
+ * 👉 not all changes that are sent to upstream are applied on the state. while
+ * the state might temporarily apply the changes on itself for performance,
+ * changes are semented to the state when they are broadcast back from the downstream.
+ *
+ */
 function getUpstream<T>(this: State<T>) { return this(_Upstream as any) as any as Upstream<T>; }
+
+/**
+ *
+ * returns a sub-state from the state. a sub-state represents the state of
+ * a particular key of the object of its parent state.
+ *
+ */
 function sub<T, K extends keyof T>(this: State<T>, k: K) {
   const _sub: SubState<T, K> = makeState(
     this.get() ? this.get()[k] : undefined,
@@ -32,19 +81,42 @@ function sub<T, K extends keyof T>(this: State<T>, k: K) {
   return _sub;
 }
 
+//
+// ⚠️ internal implementation thingy ⚠️
+//
+// a profile represents the explicit closure
+// shared between all functions of a state. I used to keep it as
+// an implicit closure, but for memory-consumption optimization made
+// it explicit.
+//
 interface Profile<T> {
-  downstream: Downstream<Change<T>>;
-  upstream: Upstream<T>;
-  sinks: Sink<Change<T>>[];
-  value: T;
-  talkback: any;
+  //
+  // TODO: shouldn't this be `Downstream<T>`? 🤦
+  //
+  downstream: Downstream<Change<T>>;  // 👉 this is the provided downstream, not state's downstream
+  upstream: Upstream<T>;              // 👉 this is the provided upstream, which is the same as state's upstream
+  sinks: Sink<Change<T>>[];           // 👉 a list of all sinks (listening to state's downstream)
+  value: T;                           // 👉 latest value of the state
+  talkback: any;                      // 👉 this is (provided) downstream talkback
 }
 
-
+//
+// ⚠️ internal implementation thingy ⚠️
+//
+// this is the standard talkback passed down to
+// all sinks of the state's downstream (downstream's greeter). it is shared
+// for better memory consumption (an individual talkback for each
+// sink consumes slightly more memory compared to a bound-function,
+// which has a measurable impact on memory consumption
+// when TONs of states / sub-states are used).
+//
 function _dsgreeter<T>(this: Profile<T>, sink: Sink<Change<T>>, type: MsgType) {
   if (type === _End) {
     const index = this.sinks.indexOf(sink);
-    if (index >= 0) { this.sinks.splice(index, 1); }
+    if (index >= 0) {
+      this.sinks.splice(index, 1);
+    }
+
     if (this.sinks.length === 0) {
       terminate(this.sinks, this.talkback);
       this.talkback = undefined;
@@ -52,34 +124,82 @@ function _dsgreeter<T>(this: Profile<T>, sink: Sink<Change<T>>, type: MsgType) {
   }
 }
 
+//
+// ⚠️ internal implementation thingy ⚠️
+//
+// represents the downstream callbag for the state, i.e. state's downstream. this is
+// basically the provided downstream, which is shared, ref counted, and:
+//
+// - keeps state's value updated with the latest change coming through the downstream
+// - post-trace's a change if the trace ends on this node. for example, if you
+//   have `{ x: { y: { z: 2, w: 3 } } }` as the parent state, then you change `y` sub
+//   to `{z: 3, w: 3}`, the original trace only goes as far as `y` sub, and without post-tracing,
+//   both `z` and `w` would have to be notified of the incoming change. with post-tracing, however,
+//   the change is traced in further depth and only `z` will be notified.
+//
+// 👉 in a cleaner code, this would also be an anonymous function within makeState()'s implicit
+// closure context. however, for more efficient memory consumption, this is kept as a shared bound function.
+// also this is why I didn't use `subject` with some added operators, since all the additional talkbacks
+// would lead to memory overheads.
+//
 function _downstream<T>(this: Profile<T>, type: MsgType, m?: any) {
   if (type === _Start) {
     const sink = m as Sink<Change<T>>;
     this.sinks.push(sink);
+
+    //
     // TODO: we can perhaps keep track of sub sinks (sinks listening for a particular sub)
     //       more efficiently, so changes are broadcast to them properly by accessing them via
     //       a hash table. this helps performance of cases with large fan-out (like big arrays).
+    //
     sink(_Start, _dsgreeter.bind(this, sink));
 
-    if (this.sinks.length === 1) {
+    if (this.sinks.length === 1) {                         // 👉 first sink, lets connect to downstream (basically ref counting here)
       this.downstream(_Start, (t: MsgType, _m?: any) => {
-        if (t === _Start) { this.talkback = _m; }
-        else if (t === _Data) {
-          const change = postTrace<T>(_m);
-          if (change.value !== this.value) { this.value = change.value!!; }
-          broadcast(_Data, change, this.sinks);
-        } else if (t === _End) { terminate(this.sinks, this.talkback, _m); this.talkback = undefined; }
+        if (t === _Start) {
+          this.talkback = _m;
+        } else if (t === _Data) {
+          const change = postTrace<T>(_m);                 // 👉 post-trace changes to know in detail what happens at further depths
+          if (change.value !== this.value) {
+            this.value = change.value!!;                   // 👉 update the state's value based on the incoming change
+          }
+          broadcast(_Data, change, this.sinks);            // 👉 broadcast the change to all sinks
+        } else if (t === _End) {
+          terminate(this.sinks, this.talkback, _m);
+          this.talkback = undefined;
+        }
       });
     }
   }
 }
 
+//
+// ⚠️ internal implementation thingy ⚠️
+//
+// this is the standard talkback provided to state's downstream for each state sink.
+// basically represents a simple map operation, passing `.value` of change objects
+// going through the downstream to value sinks, and also passing the last value on
+// greet to the sinks.
+//
 function _sgreeter<T>(this: Profile<T>, sink: Sink<T | undefined>, t: MsgType, m?: any) {
-  if (t === _Start) { sink(_Start, m); sink(_Data, this.value); }
-  else if (t === _Data) { sink(_Data, m.value); }
-  else if (t === _End) { sink(_End, m); }
+  if (t === _Start) {
+    sink(_Start, m);           // 👉 pass on greetings from state's downstream
+    sink(_Data, this.value);   // 👉 provide the sink with latest value as well
+  } else if (t === _Data) {
+    sink(_Data, m.value);      // 👉 pass on the value of incoming changes to the sink                
+  } else if (t === _End) {
+    sink(_End, m);
+  }
 }
 
+//
+// ⚠️ internal implementation thingy ⚠️
+//
+// represents the core callbag of each state.
+//
+// TODO: complete these comments.
+// TODO: shouldn't `_d` be `Downstream<T>`? 🤦
+//
 function _state<T>(this: Profile<T>, _d: Downstream<Change<T>>, type: StateMsgType, m?: any) {
   if (type === _Start) {
     const sink = m as Sink<T | undefined>;
@@ -102,6 +222,9 @@ function _state<T>(this: Profile<T>, _d: Downstream<Change<T>>, type: StateMsgTy
 }
 
 
+//
+// TODO: add comments for this fella
+//
 export function makeState<T>(initial: T, downstream: Downstream<T>, upstream: Upstream<T>): State<T>;
 export function makeState<T, K extends keyof T>
   (initial: T[K] | undefined, downstream: Downstream<T[K] | undefined>, upstream: Upstream<T[K]>): SubState<T, K>;
